7f4b60ec62277371aea65b1668159fca8952c6eb
Tobal <tobal17@gmail.com>
2016-02-15 14:58:16 +0100

    ipython: add init script for aggregation practice
    Signed-off-by: Balazs Toth <balazs.toth@balabit.com>

---
+++ languages/python/ipython/init_scripts/init_aggregate_rows.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+input_dict = {('a','b'): 1, ('a','c'): 2, ('b','c'): 3}
+index = input_dict.keys()
+data = pd.Series(input_dict, index=index).sort_values(ascending=False)
+df = pd.DataFrame(data)
+
+df.insert(0, 'ngrams', df.index)
+df = df.set_index([list(range(len(df.index)))])
+
+def should_aggregate(first_ngram, second_ngram):
+    if first_ngram[0] == second_ngram[0]:
+        return True
+    return False
