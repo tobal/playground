cc3a28da56c586107aabaa50369eaa26ee958ccc
Tobal <tobal17@gmail.com>
2016-02-17 15:45:58 +0100

    ipython: fix aggregate init script
    Signed-off-by: Tobal <tobal17@gmail.com>

--- languages/python/ipython/init_scripts/init_aggregate_rows.py
+++ languages/python/ipython/init_scripts/init_aggregate_rows.py
@@ -4,7 +4,7 @@ import numpy as np
 input_dict = {('a','b'): 1, ('a','c'): 2, ('b','c'): 3}
 index = input_dict.keys()
 data = pd.Series(input_dict, index=index).sort_values(ascending=False)
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, columns=['count'])
 
 df.insert(0, 'ngrams', df.index)
 df = df.set_index([list(range(len(df.index)))])
