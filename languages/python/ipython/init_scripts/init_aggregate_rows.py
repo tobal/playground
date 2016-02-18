cb369185c8574de18aa71b168af7992f958c373e
Tobal <tobal17@gmail.com>
2016-02-18 09:37:15 +0100

    ipython: add apply functions to init aggregate
    Signed-off-by: Tobal <tobal17@gmail.com>

--- languages/python/ipython/init_scripts/init_aggregate_rows.py
+++ languages/python/ipython/init_scripts/init_aggregate_rows.py
@@ -8,6 +8,18 @@ df = pd.DataFrame(data, columns=['count'])
 
 df.insert(0, 'ngrams', df.index)
 df = df.set_index([list(range(len(df.index)))])
+df['delete'] = False
+
+def agg_df(df):
+    for ix, (tupl, count, delete) in df.iterrows():
+        df.loc[ix+1:].apply(agg_subdf, axis=1, args=(ix, tupl, df))
+
+def agg_subdf(subdf, ix, tupl, df):
+    from ipdb import set_trace; set_trace()
+    if should_aggregate(subdf['ngrams'], tupl):
+        df.loc[ix, 'count'] += subdf['count']
+        subdf['delete'] = True
+        return subdf
 
 def should_aggregate(first_ngram, second_ngram):
     if first_ngram[0] == second_ngram[0]:
