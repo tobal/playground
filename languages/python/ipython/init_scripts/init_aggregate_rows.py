05ecb3549f50d50fac5e6ff76230f032979e09e9
Tobal <tobal17@gmail.com>
2016-04-25 22:01:29 +0200

    ipython: update aggregate rows
    Signed-off-by: Tobal <tobal17@gmail.com>

--- languages/python/ipython/init_scripts/init_aggregate_rows.py
+++ languages/python/ipython/init_scripts/init_aggregate_rows.py
@@ -1,25 +1,30 @@
 import pandas as pd
 import numpy as np
 
-input_dict = {('a','b'): 1, ('a','c'): 2, ('b','c'): 3}
+input_dict = {('a','b'): 1, ('a','c'): 2, ('b','c'): 3, ('a','g'): 1}
 index = input_dict.keys()
 data = pd.Series(input_dict, index=index).sort_values(ascending=False)
 df = pd.DataFrame(data, columns=['count'])
 
 df.insert(0, 'ngrams', df.index)
-df = df.set_index([list(range(len(df.index)))])
-df['delete'] = False
+index = list(range(len(df.index)))
+df = df.set_index([index])
+df['ix'] = index
+df['remain'] = True
 
-def agg_df(df):
-    for ix, (tupl, count, delete) in df.iterrows():
-        df.loc[ix+1:].apply(agg_subdf, axis=1, args=(ix, tupl, df))
+def aggre(df):
+    df.apply(agg_df, axis='columns', args=(df,))
 
-def agg_subdf(subdf, ix, tupl, df):
-    from ipdb import set_trace; set_trace()
-    if should_aggregate(subdf['ngrams'], tupl):
-        df.loc[ix, 'count'] += subdf['count']
-        subdf['delete'] = True
-        return subdf
+def agg_df(row, df):
+    tupl = row['ngrams']
+    ix = row['ix']
+    df.loc[ix+1:][df.remain].apply(agg_subrow, axis=1, args=(ix, tupl, df))
+
+def agg_subrow(subrow, ix, tupl, df):
+    if not subrow.empty:
+        if should_aggregate(subrow['ngrams'], tupl):
+            df.loc[ix, 'count'] += subrow['count']
+            df.loc[subrow['ix'], 'remain'] = False
 
 def should_aggregate(first_ngram, second_ngram):
     if first_ngram[0] == second_ngram[0]:
