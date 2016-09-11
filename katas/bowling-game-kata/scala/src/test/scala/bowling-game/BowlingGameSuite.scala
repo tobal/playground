8838d825c7c12e56cd5ad43aa7df95f31d63a5b1
Tobal <tobal17@gmail.com>
2016-09-12 00:21:05 +0200

    katas.bowling.scala: 4. refactor
    Signed-off-by: Tobal <tobal17@gmail.com>

--- katas/bowling-game-kata/scala/src/test/scala/bowling-game/BowlingGameSuite.scala
+++ katas/bowling-game-kata/scala/src/test/scala/bowling-game/BowlingGameSuite.scala
@@ -1,12 +1,19 @@
 import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.BeforeAndAfterAllConfigMap
+import org.scalatest.ConfigMap
 
 import BowlingGame._
 
-class BowlingGameTest extends FunSpec with ShouldMatchers {
+class BowlingGameTest extends FunSpec with ShouldMatchers with BeforeAndAfterAllConfigMap {
+    private var g: BowlingGame = new BowlingGame
+
+    override def beforeAll(configMap: ConfigMap) {
+        g = new BowlingGame
+    }
+
     describe("BowlingGame") {
         it ("scores zero on gutter game") {
-            val g: BowlingGame = new BowlingGame
             for {
                 i <- 0 until 20
             } yield g.roll(0)
@@ -14,7 +21,6 @@ class BowlingGameTest extends FunSpec with ShouldMatchers {
         }
 
         it ("scores 20 on all ones game") {
-            val g: BowlingGame = new BowlingGame
             for {
                 i <- 0 until 20
             } yield g.roll(1)
