bdb78b1cc738d6ca48fa14b318b3fa2fbee723e1
Tobal <tobal17@gmail.com>
2016-09-12 11:33:40 +0200

    katas.bowling.scala: 5. green
    Signed-off-by: Tobal <tobal17@gmail.com>

--- katas/bowling-game-kata/scala/src/main/scala/bowling-game/BowlingGame.scala
+++ katas/bowling-game-kata/scala/src/main/scala/bowling-game/BowlingGame.scala
@@ -4,11 +4,25 @@ package BowlingGame
 class BowlingGame {
     var rolls: List[Int] = List()
 
+    private def frameScore(frameIndex: Int): Int = {
+        var sc = 0
+        val idx = frameIndex * 2
+        if (rolls(idx) + rolls(idx + 1) == 10)
+            sc += 10 + rolls(idx + 2)
+        else
+            sc += rolls(idx) + rolls(idx + 1)
+        sc
+    }
+
     def roll(pins: Int): Any = {
         rolls = rolls :+ pins
     }
 
     def score: Int = {
-        rolls.foldLeft(0)(_ + _)
+        var sc = 0
+        for {
+            i <- 0 until 10
+        } yield sc += frameScore(i)
+        sc
     }
 }
