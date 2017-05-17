e2b8f88993e4f9a30f32d26b8c893f29195ac857
Tobal <tobal17@gmail.com>
2017-05-17 22:11:20 +0200

    haskell: day 6 hptr: pi with where
    Signed-off-by: Tobal <tobal17@gmail.com>

--- languages/haskell/intro_to_haskell_hptr/Pi.hs
+++ languages/haskell/intro_to_haskell_hptr/Pi.hs
@@ -2,9 +2,9 @@
 module Main where
 
 cylinderVolume :: Double -> Double -> Double
-cylinderVolume radius height = let square x = x * x
-                                   baseArea = (square radius) * pi
-                                in baseArea * height
+cylinderVolume radius height = baseArea * height
+    where square x = x * x
+          baseArea = (square radius) * pi
 
 computePi :: Double
 computePi = cylinderVolume 1.0 1.0
