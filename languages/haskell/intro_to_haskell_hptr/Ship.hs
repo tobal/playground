586027b98bb20afae34993955af46ad37be30483
Tobal <tobal17@gmail.com>
2017-05-18 16:42:02 +0200

    haskell: day 8 hptr: Ship extractor functions
    Signed-off-by: Tobal <tobal17@gmail.com>

--- languages/haskell/intro_to_haskell_hptr/Ship.hs
+++ languages/haskell/intro_to_haskell_hptr/Ship.hs
@@ -5,3 +5,12 @@ data Propulsion = Wind
                 | Diesel
 
 data Ship = S Propulsion Double Int
+
+propulsion :: Ship -> Propulsion
+propulsion (S propulsion lengthFeet passengerCap) = propulsion
+
+lengthFeet :: Ship -> Double
+lengthFeet (S propulsion lengthFeet passengerCap) = lengthFeet
+
+passengerCap :: Ship -> Int
+passengerCap (S propulsion lengthFeet passengerCap) = passengerCap
