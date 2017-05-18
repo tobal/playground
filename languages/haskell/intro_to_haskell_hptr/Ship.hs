dfc3fadf85d143cacd138bf171967d33274a738b
Tobal <tobal17@gmail.com>
2017-05-18 17:27:57 +0200

    haskell: day 8 hptr: Ship extractors with record syntax
    Signed-off-by: Tobal <tobal17@gmail.com>

--- languages/haskell/intro_to_haskell_hptr/Ship.hs
+++ languages/haskell/intro_to_haskell_hptr/Ship.hs
@@ -4,13 +4,4 @@ module Ship where
 data Propulsion = Wind
                 | Diesel
 
-data Ship = S Propulsion Double Int
-
-propulsion :: Ship -> Propulsion
-propulsion (S propulsion lengthFeet passengerCap) = propulsion
-
-lengthFeet :: Ship -> Double
-lengthFeet (S propulsion lengthFeet passengerCap) = lengthFeet
-
-passengerCap :: Ship -> Int
-passengerCap (S propulsion lengthFeet passengerCap) = passengerCap
+data Ship = S { propulsion :: Propulsion, lengthFeet :: Double, passengerCap :: Int }
