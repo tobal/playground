bd2b7825fa526724d2f42524e03ee59471077a0f
Tobal <tobal17@gmail.com>
2017-06-06 14:59:21 +0200

    haskell: day 10 hptr: polymorphism exercise
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ languages/haskell/intro_to_haskell_hptr/lecture10/polymorphism.hs
@@ -0,0 +1,21 @@
+
+identityInt :: Int -> Int
+identityInt x = x
+
+identityString :: String -> String
+identityString x = x
+
+identity :: a -> a
+identity x = x
+
+fun1 :: a -> b -> a
+fun1 x y = x
+
+fun2 :: a -> b -> b
+fun2 x y = y
+
+fun3a :: a -> a -> a
+fun3a x y = x
+
+fun3b :: a -> a -> a
+fun3b x y = y
