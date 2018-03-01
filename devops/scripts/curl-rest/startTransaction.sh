ec9554e9f1ab7a7a1bf33abf0b9700dc22ece790
Tobal <tobal17@gmail.com>
2018-03-01 13:32:30 +0100

    devops/scripts/curl-rest/startTransaction: rename session file
    Signed-off-by: Tobal <tobal17@gmail.com>

--- devops/scripts/curl-rest/startTransaction.sh
+++ devops/scripts/curl-rest/startTransaction.sh
@@ -3,6 +3,6 @@
 if [ -z "$1" ]; then echo "please specify an address"; exit -1;
 fi
 IP=$1
-curl  --digest --user admin:a --cookie-jar cookies -k --basic https://$IP/api/authentication
-SESSION_ID=`cat cookies | grep session_id | cut -f7`
+curl  --digest --user admin:a --cookie-jar .session -k --basic https://$IP/api/authentication
+SESSION_ID=`cat .session | grep session_id | cut -f7`
 curl https://$IP/api/transaction --request POST -k --cookie session_id=$SESSION_ID --data ''
