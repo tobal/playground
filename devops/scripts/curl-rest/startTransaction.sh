6161844e95ab5002b35ad8fff34977a1e500d619
Tobal <tobal17@gmail.com>
2018-03-01 10:30:07 +0100

    devops/scripts/curl-rest/startTransaction: add parameter check
    Signed-off-by: Tobal <tobal17@gmail.com>

--- devops/scripts/curl-rest/startTransaction.sh
+++ devops/scripts/curl-rest/startTransaction.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then echo "please specify an address"; exit -1;
+fi
 IP=$1
 curl  --digest --user admin:a --cookie-jar cookies -k --basic https://$IP/api/authentication
 SESSION_ID=`cat cookies | grep session_id | cut -f7`
 curl https://$IP/api/transaction --request POST -k --cookie session_id=$SESSION_ID --data ''
-rm -rf cookies
