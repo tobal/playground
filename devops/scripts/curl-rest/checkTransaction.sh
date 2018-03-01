05894d1e8d1095484179170840278395ea705801
Tobal <tobal17@gmail.com>
2018-03-01 15:14:22 +0100

    devops/scripts/curl-rest/checkTransaction: extract status from json
    Signed-off-by: Tobal <tobal17@gmail.com>

--- devops/scripts/curl-rest/checkTransaction.sh
+++ devops/scripts/curl-rest/checkTransaction.sh
@@ -4,4 +4,4 @@ if [ -z "$1" ]; then echo "please specify an address"; exit -1;
 fi
 IP=$1
 SESSION_ID=`cat .session | grep session_id | cut -f7`
-curl https://$IP/api/transaction --request GET -k --cookie session_id=$SESSION_ID
+curl https://$IP/api/transaction --request GET -k --cookie session_id=$SESSION_ID | jq ".body.status"
