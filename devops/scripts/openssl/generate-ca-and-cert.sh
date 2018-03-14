fb6ab3a7856562dbd677afe4c73af3406e11526e
Tobal <tobal17@gmail.com>
2018-03-14 13:57:11 +0100

    devops/scripts/openssl: generate client certs too
    Signed-off-by: Tobal <tobal17@gmail.com>

--- devops/scripts/openssl/generate-ca-and-cert.sh
+++ devops/scripts/openssl/generate-ca-and-cert.sh
@@ -15,3 +15,7 @@ openssl req -x509 -config ../openssl.cnf -newkey rsa:2048 -days 365 -out cacert.
 openssl genrsa -out server_key.pem 2048
 openssl req -new -key server_key.pem -out server_req.pem -outform PEM -subj /CN=changeme_hostname/O=server/ -nodes
 openssl ca -config ../openssl.cnf -in server_req.pem -out server_cert.pem -notext -batch -extensions server_ca_extensions
+
+openssl genrsa -out client_key.pem 2048
+openssl req -new -key client_key.pem -out client_req.pem -outform PEM -subj /CN=changeme_hostname/O=client/ -nodes
+openssl ca -config ../openssl.cnf -in client_req.pem -out client_cert.pem -notext -batch -extensions client_ca_extensions
