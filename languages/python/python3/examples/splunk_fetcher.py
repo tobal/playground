1d34bb3fad8dc8eb0ee824b7ad2ed3949fe31f4a
Tobal <tobal17@gmail.com>
2018-12-11 14:55:21 +0100

    python3/examples/splunk_fetcher: try requests package, use python3 interpreter
    Signed-off-by: Tobal <tobal17@gmail.com>

--- languages/python/python3/examples/splunk_fetcher.py
+++ languages/python/python3/examples/splunk_fetcher.py
@@ -1,33 +1,47 @@
-#!/usr/bin/python -u
+#!/usr/bin/env python3
 
-import urllib
-import httplib2
+
+import requests
+from sys import argv
 from xml.dom import minidom
 
-baseurl = 'https://localhost:8089'
-userName = 'admin'
-password = 'password'
 
-searchQuery = '| inputcsv foo.csv | where sourcetype=access_common | head 5'
+if len(argv) < 4:
+    print('Please specify the base url, username and password')
+    exit(1)
+
+baseurl, username, password = argv[1], argv[2], argv[3]
+
+baseurl = 'https://10.170.30.100:8089'
+username = 'admin'
+password = 'TitkoS12'
+
+
+def get_search_query():
+    search_query = 'host="zion" index="local_var_log" process="sshd"'
+    if not (search_query.startswith('search') or search_query.startswith("|")):
+        search_query = 'search ' + search_query
+    print(search_query)
+    return search_query
+
+
+auth_params = {'username':username, 'password':password}
+server_content = requests.post(baseurl + '/services/auth/login', params=auth_params)
+print(server_content)
+
 
 # Authenticate with server.
 # Disable SSL cert validation. Splunk certs are self-signed.
-serverContent = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/auth/login',
-    'POST', headers={}, body=urllib.urlencode({'username':userName, 'password':password}))[1]
+#serverContent = httplib2.Http(disable_ssl_certificate_validation=True).request(
+#                    baseurl + '/services/auth/login', 'POST', headers={},
+#                    body=urllib.urlencode({'username':username, 'password':password}))[1]
 
-sessionKey = minidom.parseString(serverContent).getElementsByTagName('sessionKey')[0].childNodes[0].nodeValue
 
-# Remove leading and trailing whitespace from the search
-searchQuery = searchQuery.strip()
 
-# If the query doesn't already start with the 'search' operator or another 
-# generating command (e.g. "| inputcsv"), then prepend "search " to it.
-if not (searchQuery.startswith('search') or searchQuery.startswith("|")):
-    searchQuery = 'search ' + searchQuery
+#sessionKey = minidom.parseString(serverContent).getElementsByTagName('sessionKey')[0].childNodes[0].nodeValue
 
-print searchQuery
 
 # Run the search.
 # Again, disable SSL cert validation. 
-print httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs','POST',
-    headers={'Authorization': 'Splunk %s' % sessionKey},body=urllib.urlencode({'search': searchQuery}))[1]
+#print(httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs','POST',
+#    headers={'Authorization': 'Splunk %s' % sessionKey},body=urllib.urlencode({'search': searchQuery}))[1])
