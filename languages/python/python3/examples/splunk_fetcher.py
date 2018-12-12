40a3cd076d79bdcdeadd37dc9401bb0ba6d315ce
Tobal <tobal17@gmail.com>
2018-12-12 12:23:38 +0100

    python3/examples/splunk_fetcher: refactor into functions
    Signed-off-by: Tobal <tobal17@gmail.com>

--- languages/python/python3/examples/splunk_fetcher.py
+++ languages/python/python3/examples/splunk_fetcher.py
@@ -8,10 +8,10 @@ import urllib3
 
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 if len(argv) < 4:
     print('Please specify the base url, username and password')
     exit(1)
-
 baseurl, username, password = argv[1], argv[2], argv[3]
 
 
@@ -22,17 +22,25 @@ def get_search_query():
     return search_query
 
 
-server_content = requests.post(baseurl + '/services/search/jobs',
-                               auth=(username, password),
-                               verify=False,
-                               data={'search': get_search_query()})
-search_id = minidom.parseString(server_content.text).getElementsByTagName('sid')[0].firstChild.nodeValue
-
-while True:
-    search_results = requests.get(baseurl + '/services/search/jobs/' + search_id + '/results/',
+def run_search_and_get_id():
+    server_content = requests.post(baseurl + '/services/search/jobs',
                                    auth=(username, password),
                                    verify=False,
-                                   data={'output_mode': 'json'})
-    if search_results.status_code == 200:
-        print(search_results.text)
-        break
+                                   data={'search': get_search_query()})
+    return minidom.parseString(server_content.text).getElementsByTagName('sid')[0].firstChild.nodeValue
+
+
+def main():
+    search_id = run_search_and_get_id()
+    while True:
+        search_results = requests.get(baseurl + '/services/search/jobs/' + search_id + '/results/',
+                                      auth=(username, password),
+                                      verify=False,
+                                      data={'output_mode': 'json'})
+        if search_results.status_code == 200:
+            print(search_results.text)
+            break
+
+
+if __name__ == '__main__':
+    main()
