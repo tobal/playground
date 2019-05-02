5c166dd16d8fcab70518a18d229691b648dbf7ce
Tobal <tobal17@gmail.com>
2019-05-03 01:35:05 +0200

    languages/pytho3: use exception for error
    Signed-off-by: Tobal <tobal17@gmail.com>

--- languages/python/python3/identify_sublist.py
+++ languages/python/python3/identify_sublist.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 #
-# The program does not take input parameters, just runs
+# The program does not take command line parameters, just runs
 # on the example test data. To feed it with your own data,
-# you should use it as a module in your own script, like so:
-#
-# from identify_sublist import identify_sublist
-# 
+# you should use it as a module in your own script. Import
+# the function and it's exception class, and put the call
+# in a try block to handle any errors.
+
 
 import sys
 from operator import itemgetter
+
+
+class InputError(Exception):
+    pass
 
 
 def identify_sublist(digit_charlist):
@@ -21,26 +25,19 @@ def identify_sublist(digit_charlist):
             for idx_end in range(idx_begin, len(digit_charlist))
             if same_digit_char_num(digit_charlist[idx_begin:idx_end + 1])]
     if not candidate_sublists_and_lengths:
-        print("No candidate sublists")
-        sys.exit(1)
+        raise InputError("No candidate sublists")
     indexes_of_longest_sublist = max(candidate_sublists_and_lengths, key=itemgetter(1))[0]
     return indexes_of_longest_sublist
-
-    # Here is the same code which I wrote first, but wanted to get rid of the
-    # state variables. This one is easier to read though
-    #max_begin = max_end = 0
-    #for idx_begin in range(len(digit_charlist)):
-    #    for idx_end in range(len(digit_charlist)):
-    #        if same_digit_char_num(digit_charlist[idx_begin:idx_end + 1]):
-    #            if ((idx_end - idx_begin) > (max_end - max_begin)):
-    #                max_begin = idx_begin
-    #                max_end = idx_end
-    #return (max_begin, max_end)
 
 
 if __name__ == "__main__":
     test_charlist = [0, 'a', 'c', 4, 1, 2, 'b', 0, 2, 3]
-    (idx_begin, idx_end) = identify_sublist(test_charlist)
+    try:
+        (idx_begin, idx_end) = identify_sublist(test_charlist)
+    except InputError as exc:
+        print(exc)
+        sys.exit(1)
+
     assert idx_begin == 1
     assert idx_end == 6
     sys.exit(0)
