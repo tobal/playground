f7752ac4bcedc10028ec36dfa3676a20b624f3a5
Balazs Toth <balazs.toth@balabit.com>
2019-11-06 10:50:49 +0100

    devops/curl-rest: add script to delete transaction
    Signed-off-by: Balazs Toth <balazs.toth@balabit.com>

---
+++ devops/scripts/curl-rest/deleteTransaction.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then echo "please specify an address"; exit -1;
+fi
+IP=$1
+SESSION_ID=`cat .session | grep session_id | cut -f7`
+curl https://$IP/api/transaction --request DELETE -k --cookie session_id=$SESSION_ID
